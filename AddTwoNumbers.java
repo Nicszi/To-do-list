d24e6dab54c4ff40eeed4639edf4a6885449e07e
Carlo Eugene Manalo <carloeugene@gmail.com>
2023-06-22 17:37:30 +0800

    Added new program

---
+++ AddTwoNumbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2, b = 6, sum;
+        sum = a + b;
+        System.out.println("Sum of numbers is:" +sum);
+        }
+    }
